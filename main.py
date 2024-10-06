f7bbaacb9a418fb459f0babba414db13195f98d1
pushpendra-saini-pks <pk.karauli2000@gmail.com>
2024-10-06 14:43:29 +0530

    final comit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 from typing import Optional ,List
 
@@ -11,7 +11,9 @@ class Item(BaseModel):
     price : float
     is_offer : Optional[bool] = None
     
-    
+
+## IN memory-database 
+items = {}
     
 # define a basic route 
 @app.get("/")
@@ -35,3 +37,34 @@ def create_item(item:Item):
     return {"item_name":item.name,"item_price" : item.price, "item_is_offer":item.is_offer}
 
 
+@app.post("/items", response_model=Item)
+def create_item(item:Item ,item_id:int):
+    if item_id in items : 
+        raise HTTPException(status_code=400,detail = "Item already exists")
+    items[item_id] = item
+    return item
+
+
+
+@app.get("/items", response_model=Item)
+def create_item(item_id:int):
+    if item_id not in items : 
+        raise HTTPException(status_code=400,detail = "Item Not exists")
+    
+    return items[item_id]
+
+
+@app.put("/items", response_model=Item)
+def update_item(item_id:int, updated_item:Item):
+    if item_id not in items : 
+        raise HTTPException(status_code=400,detail = "Item Not exists")
+    items[item_id] = update_item
+    return update_item
+
+
+@app.delete("/items/{item_id}", response_model=Item)
+def delete_item(item_id:int):
+    if item_id not in items : 
+        raise HTTPException(status_code=400,detail = "Item already exists")
+    deleted_item = items.pop(item_id)
+    return deleted_item
